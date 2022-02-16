6db407da8d2824a707b44895d170ac38ca8a3504
Elijah Seth Medrano <97810174+esmedrano@users.noreply.github.com>
2022-02-16 13:12:23 -0600

    Update stone_of_philosophers.py

--- stone_of_philosophers.py
+++ stone_of_philosophers.py
@@ -27,23 +27,37 @@ class Regular_Polygon:
   def draw(self):
     rotation_ang =  2 * pi / self.side_num
     endpoint = [0, 0]
+    angle_count = 0
     for _ in [*range(0, self.side_num)]:
-      endpoint[0] += self.side_len * cos(rotation_ang)
-      endpoint[1] += self.side_len * sin(rotation_ang)
+      if angle_count < 90:
+        endpoint[0] = self.pos[0] - self.side_len * cos(rotation_ang)
+        endpoint[1] = self.pos[1] - self.side_len * sin(rotation_ang)
+      if angle_count > 90:
+        pass
       pg.draw.line(self.surface, self.color, self.pos, endpoint)
+      # self.pos = endpoint[:]
+      angle_count += rotation_ang
   
 
 def stone_of_philosophers(center_radius):
-  triangle = Regular_Polygon(window, RED, 3, (CENTER[0] + sqrt((2 * center_radius) ** 2), CENTER[1] - center_radius ** 2 + 2 * center_radius), 2 * center_radius + 2 * center_radius / SQRT_3 * 2)
+  a = center_radius
+  c = center_radius * (SQRT_3 + 1)
+  b = sqrt(c ** 2 - a ** 2)
+  start_point = (CENTER[0] + b, CENTER[1] + center_radius)
+  x = center_radius * 2
+  y = b - center_radius
+  z = sqrt(a ** 2 + b ** 2)
+  side_len = (center_radius * 2 + z)
+  triangle = Regular_Polygon(window, RED, 3, start_point, side_len)
   pg.draw.circle(window, BLUE, CENTER, center_radius, 1)
   pg.draw.rect(window, GREEN, (CENTER[0] - center_radius, CENTER[1] - center_radius, 
                                center_radius * 2, center_radius * 2), 1)
   triangle.draw()
-  pg.draw.circle(window, WHITE, CENTER, 2 * center_radius, 1)
+  pg.draw.circle(window, WHITE, CENTER, int(center_radius * (SQRT_3 + 1)), 1)
 
 
 def main():
-  radius = 100
+  radius = 50
   isStoned = False
   while 1:
     for event in pg.event.get():
@@ -57,4 +71,3 @@ def main():
 
 if __name__ == '__main__':
   main()
-
